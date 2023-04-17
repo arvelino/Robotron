c26342d019046722f05ea050ab6cd79e4af3cf47
Robson Arvelino <rob.arvelino@gmail.com>
2023-04-17 18:25:24 -0300

    Atualizado o contador
    Atualizado a formula do contador de estatística.

--- js/main.js
+++ js/main.js
@@ -36,38 +36,46 @@ const pecas = {
         "velocidade": -2
     }
 }
+let validador=0;
 // console.log(estatisticas);
 
 controle.forEach( (elemento) => {
-    elemento.addEventListener('click', (evento) => {
-        manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
 
-        atualizaEstatisticas(evento.target.dataset.peca,evento.target.dataset.controle);
+    elemento.addEventListener('click', (evento) => {        
+        const contador = evento.target.parentNode.querySelector("[data-contador]");
+        validador = parseInt(contador.value);
+
+        manipulaDados(evento.target.dataset.controle, contador);
+      
+        atualizaEstatisticas(evento.target.dataset.peca,evento.target.dataset.controle,validador);      
+        
     })
     
 })
 
+/* Função para alterar os controle, somando e subtraindo do menu */
 function manipulaDados(operacao,controle) {
-    const peça = controle.querySelector("[data-contador]");
 
-    if(operacao === "-" && peça.value > 0) {
-        peça.value = parseInt(peça.value) - 1
+    if(operacao === "-" && controle.value > 0) {
+        controle.value = parseInt(controle.value) - 1
     } else if(operacao === "+"){ 
-        peça.value = parseInt(peça.value) + 1
+        controle.value = parseInt(controle.value) + 1
     }
 
 }
 
+/*  Função para somar as caracteristicas na estatistica do Robo */
+function atualizaEstatisticas(peca,operacao,validar){
+        // console.log(parseInt(validar));
 
-function atualizaEstatisticas(peca,operacao){
-        estatisticas.forEach((elemento)=>{
-          
-            if(operacao ==="-" ){
+        estatisticas.forEach((elemento)=>{ 
+
+            if(operacao ==="-" && validar>0){
                 elemento.textContent = parseInt(elemento.textContent) - pecas[peca][elemento.dataset.estatisticas];
                 
-            }else{
+            }else if(operacao ==='+'){
                 elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatisticas];
-            }            
+            }  
     })   
 
 }
@@ -79,7 +87,7 @@ function validaProducao(){
     let indice = 0;
     estatisticas.forEach((elemento)=>{    
         if(Number(elemento.textContent)==0){
-            indice +=1;
+            indice ++;
         }
 
     })
